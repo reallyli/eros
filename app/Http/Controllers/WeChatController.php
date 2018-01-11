12a0b02eb4677347ff956eb3d09990004897740e
reallyli <lizhuangzhuang@glz8.com>
2018-01-11 09:45:45 +0800

    消息分类回复

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -16,55 +16,79 @@ use Illuminate\Support\Facades\Log;
  */
 class WeChatController extends Controller
 {
-     /**
-      * Method description:server
-      *
-      * @author reallyli <zlisreallyli@outlook.com>
-      * @throws \Exception
-      * @return mixed
-      * 返回值类型：string，array，object，mixed（多种，不确定的），void（无返回值）
-      */
-     public function server()
-     {
+    /**
+     * Method description:server
+     *
+     * @author reallyli <zlisreallyli@outlook.com>
+     * @throws \Exception
+     * @return mixed
+     * 返回值类型：string，array，object，mixed（多种，不确定的），void（无返回值）
+     */
+    public function server()
+    {
         Log::info('request arrived.');
         try {
             $app = app('wechat.official_account');
             $app->server->push(function ($message) {
-                return "终于等到你 ！";
+                switch ($message['MsgType']) {
+                    case 'event':
+                        return '你到底在干啥？';
+                        break;
+                    case 'text':
+                        return date('Y-m-d', $message['CreateTime']) . '是个值得纪念的日子终于等到你';
+                        break;
+                    case 'image':
+                        return '图片链接是' . $message['PicUrl'];
+                        break;
+                    case 'voice':
+                        return '你说的什么我听不清楚!但是我知道这个语音的格式是' . $message['Format'];
+                        break;
+                    case 'video':
+                        return '你发的什么玩意儿？视频消息媒体ID' . $message['MediaId'];
+                        break;
+                    case 'location':
+                        return '哈哈 我知道你在哪里了，位置是经度：' . $message['Longitude'] . '，维度：' . $message['Latitude'] . ", 信息是" . $message['Label'];
+                        break;
+                    case 'link':
+                        return '你这个链接我懂得，消息标题是' . $message['Title'] . '，消息描述是' . $message['Description'] . '，消息链接是' . $message['Url'];
+                        break;
+                    // ... 其它消息
+                    default:
+                        return '终于等到你';
+                        break;
+                }
             });
 
             return $app->server->serve();
         } catch (\Exception $e) {
             throw $e;
         }
-     }
+    }
 
+    public function valid(Request $request)
+    {
+        Log::error(json_encode($_REQUEST));
+        $echoStr = $request->input('echostr');
+        if ($this->checkSignature($request)) {
+            echo $echoStr;
+            exit;
+        }
+    }
 
-     public function valid(Request $request)
-     {
-         Log::error(json_encode($_REQUEST));
-         $echoStr = $request->input('echostr');
-         if($this->checkSignature($request)){
-             echo $echoStr;
-             exit;
-         }
-     }
-
-     private function checkSignature($request)
-     {
-         $signature = $request->input('signature');
-         $timestamp = $request->input('timestamp');
-         $nonce = $request->input('nonce');
-         $token = 'kerry0709';
-         $tmpArr = array($token, $timestamp, $nonce);
-         sort($tmpArr);
-         $tmpStr = implode( $tmpArr );
-         $tmpStr = sha1( $tmpStr );
-         if( $tmpStr == $signature ) {
-             return true;
-         } else {
-             return false;
-         }
-     }
- }
-
+    private function checkSignature($request)
+    {
+        $signature = $request->input('signature');
+        $timestamp = $request->input('timestamp');
+        $nonce = $request->input('nonce');
+        $token = 'kerry0709';
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+        if ($tmpStr == $signature) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
