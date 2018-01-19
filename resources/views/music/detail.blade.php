5f63798cfcc5825039d22cbe4fd63853baee80c4
reallyli <lizhuangzhuang@glz8.com>
2018-01-19 13:16:20 +0800

    update background color

--- resources/views/music/detail.blade.php
+++ resources/views/music/detail.blade.php
@@ -1,47 +1,42 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="/node_modules/materialize-css/dist/css/materialize.min.css"  media="screen,projection"/>
-    <!--Let browser know website is optimized for mobile-->
+    <link type="text/css" rel="stylesheet" href='/css/detail.css' />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   </head>
   <body>
-
     <div class="container">
-            <div class="col s12 m5">
-                <div class="card hoverable">
-                    <div class="card-image">
-                        <img src="{{$musicDetail['songs'][0]['al']['picUrl']}}">
-                        <span class="card-title"><b>{{$musicDetail['songs'][0]['name']}}</b></span>
-                    </div>
-                    <div id="aplayer1" class="aplayer">
-                    </div>
-                    <div class="card-content">
-                        @if($comment['hotComments'])
-                            @foreach($comment['hotComments'] as $commentDetail)
-                                <div class="divider"></div>
-                                <div class="section">
-                                    <h5>
-                                        <div class="chip">
-                                            <img src="{{$commentDetail['user']['avatarUrl']}}" alt="{{$commentDetail['user']['nickname']}}">
-                                            {{$commentDetail['user']['nickname']}}
-                                        </div>
-                                        <span class="secondary-content">{{$commentDetail['likedCount']}}<i class="material-icons">star_border</i></span>
-                                    </h5>
-                                    <p>
-                                        {{$commentDetail['content']}}
-                                    </p>
-                                </div>
-                            @endforeach
-                        @endif
-                    </div>
+        <div class="col s12 m5">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="{{$musicDetail['songs'][0]['al']['picUrl']}}">
+                    <span class="card-title"><b>{{$musicDetail['songs'][0]['name']}}</b></span>
+                </div>
+                <div id="aplayer1" class="aplayer"></div>
+                <div class="card-content">
+                    @if($comment['hotComments'])
+                        @foreach($comment['hotComments'] as $commentDetail)
+                            <div class="divider"></div>
+                            <div class="section">
+                                <h5>
+                                    <div class="chip">
+                                        <img src="{{$commentDetail['user']['avatarUrl']}}" alt="{{$commentDetail['user']['nickname']}}">
+                                        {{$commentDetail['user']['nickname']}}
+                                    </div>
+                                    <span class="secondary-content">{{$commentDetail['likedCount']}}<i class="material-icons">star_border</i></span>
+                                </h5>
+                                <p>
+                                    {{$commentDetail['content']}}
+                                </p>
+                            </div>
+                        @endforeach
+                    @endif
                 </div>
             </div>
+        </div>
     </div>
-    <!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="/node_modules/materialize-css/dist/js/materialize.min.js"></script>
     <script type="text/javascript" src="/node_modules/aplayer/dist/APlayer.min.js"></script>
